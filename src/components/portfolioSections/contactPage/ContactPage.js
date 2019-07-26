4f05f0fe3fe3ce7feafc066a68501187bce9edd7
Matt Aiken <mattaiken@hotmail.co.uk>
2019-07-26 13:55:09 +0100

    responsive style changes

--- src/components/portfolioSections/contactPage/ContactPage.js
+++ src/components/portfolioSections/contactPage/ContactPage.js
@@ -6,38 +6,45 @@ import Footer from '../../footer/Footer';
 const ContactPage = () => {
   return (
     <section id="contact">
-      <h1 className="contact-header">Contact</h1>
-      <p className="contact-desc">
-        Please fill out the form below with any queries. I'd love to hear from
-        you.
-      </p>
       <form
         className="contact-form"
         name="contact"
         method="POST"
         data-netlify="true"
       >
-        <div>
-          <input type="text" name="name" id="name" placeholder="Name" />
+        <div className="header-desc">
+          <h1 className="contact-header">Contact</h1>
+          <p className="contact-desc">
+            Please fill out the form with any queries. I'd love to hear from
+            you.
+          </p>
         </div>
-        <div>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email address"
-          />
+        <div className="contact-inputs">
+          <div>
+            <input type="text" name="name" id="name" placeholder="Name" />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email address"
+            />
+          </div>
         </div>
-        <label htmlFor="message">Message</label>
-        <div>
-          <textarea name="message" id="message" rows="10" />
+        <div className="textbox">
+          <label htmlFor="message">Message</label>
+          <div>
+            <textarea name="message" id="message" rows="9" />
+          </div>
         </div>
-        <div data-netlify-recaptcha="true" />
-        <div className="submit-button">
-          <button type="submit">Submit</button>
+        <div className="button-container">
+          <div data-netlify-recaptcha="true" />
+          <div className="submit-button">
+            <button type="submit">Submit</button>
+          </div>
         </div>
       </form>
-
       <Footer />
     </section>
   );
